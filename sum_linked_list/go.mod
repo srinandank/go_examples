e69dda10cfd3d4cb9c88acf1cc051f1331babef3
Srinandan Krishnamoorthy <km.srinandan@gmail.com>
2026-06-03 23:22:44 +0200

    Update sum linked lists package

--- sum_linked_list/go.mod
+++ sum_linked_list/go.mod
@@ -1,3 +1,5 @@
 module github.com/srinandank/go_examples/sum_linked_list
 
 go 1.26.3
+
+require github.com/srinandank/go_examples/linked_lists v0.0.0-20260603211733-ce76afbe2963
--- sum_linked_list/sum_linked_list.go
+++ sum_linked_list/sum_linked_list.go
@@ -1,39 +1,39 @@
 package main
 
-import "github.com/srinandank/go_examples/linked_lists"
+import linkedlists "github.com/srinandank/go_examples/linked_lists"
 
-func sum_linked_list(list1 *linked_lists.LinkedList, list2 *linked_lists.LinkedList) linked_lists.LinkedList {
+func sum_linked_list(list1 *linkedlists.LinkedList, list2 *linkedlists.LinkedList) linkedlists.LinkedList {
 	last_first := list1.Head
 	last_second := list2.Head
-	var new_linked_list linked_lists.LinkedList
+	var new_linked_list linkedlists.LinkedList
 	quotient := 0
 	for last_first != nil || last_second != nil {
 		sum := 0
 		if last_first != nil {
-			sum += last_first.value
-			last_first = last_first.next
+			sum += last_first.Value
+			last_first = last_first.Next
 		}
 		if last_second != nil {
-			sum += last_second.value
-			last_second = last_second.next
+			sum += last_second.Value
+			last_second = last_second.Next
 		}
 		sum += quotient
 		remainder := sum % 10
 		quotient = sum / 10
-		linked_lists.AppendToLinkedList(&new_linked_list, remainder)
+		linkedlists.AppendToLinkedList(&new_linked_list, remainder)
 	}
 	return new_linked_list
 }
 
 func main() {
-	list1 := linked_lists.CreateLinkedList([]int{1, 1, 3, 5, 8})
-	list2 := linked_lists.CreateLinkedList([]int{5, 3, 8, 13})
+	list1 := linkedlists.CreateLinkedList([]int{1, 1, 3, 5, 8})
+	list2 := linkedlists.CreateLinkedList([]int{5, 3, 8, 13})
 
-	linked_lists.PrintLinkedList("First list", &list1)
-	linked_lists.PrintLinkedList("Second list", &list2)
+	linkedlists.PrintLinkedList("First list", &list1)
+	linkedlists.PrintLinkedList("Second list", &list2)
 
 	output_linked_list := sum_linked_list(&list1, &list2)
 
-	linked_lists.PrintLinkedList("Output List", &output_linked_list)
+	linkedlists.PrintLinkedList("Output List", &output_linked_list)
 
 }
